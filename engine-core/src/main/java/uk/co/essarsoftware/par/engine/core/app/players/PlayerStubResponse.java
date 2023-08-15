f064ce0bf4081f914ab05142138445c622875e97
Steve Roberts <steve.roberts@essarsoftware.co.uk>
2023-08-15 10:38:08 +0100

    refactor: introduce PlayerStubResponse class

---
+++ engine-core/src/main/java/uk/co/essarsoftware/par/engine/core/app/players/PlayerStubResponse.java
@@ -0,0 +1,38 @@
+package uk.co.essarsoftware.par.engine.core.app.players;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+
+import uk.co.essarsoftware.par.game.Player;
+
+public class PlayerStubResponse
+{
+
+    private final Player player;
+
+    public PlayerStubResponse(Player player) {
+
+        this.player = player;
+
+    }
+
+    @JsonGetter("player_id")
+    public String getPlayerID() {
+
+        return player.getPlayerID();
+
+    }
+
+    @JsonGetter("player_name")
+    public String getPlayerName() {
+
+        return player.getPlayerName();
+
+    }
+
+    @JsonGetter("player_state")
+    public String getPlayerState() {
+
+        return player.getPlayerState().name();
+
+    }
+}
